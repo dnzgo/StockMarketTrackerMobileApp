e955a186fdf0109400849ad91fbcc72fec88c82b
emiryalcinkaya <emirylk06@gmail.com>
2026-06-06 22:06:55 +0200

    Update main_navigation_screen.dart

--- lib/screens/main_navigation_screen.dart
+++ lib/screens/main_navigation_screen.dart
@@ -16,7 +16,7 @@ class MainNavigationScreen extends StatefulWidget {
 class _MainNavigationState extends State<MainNavigationScreen> {
   int selectedIndex = 0;
 
-  final List<Widget> screens = [
+  final List<Widget> screens = const [
     HomeScreen(),
     NewsExploreScreen(),
     StockExploreScreen(),
@@ -31,42 +31,76 @@ class _MainNavigationState extends State<MainNavigationScreen> {
         center: Alignment.bottomRight,
       ),
       child: Scaffold(
+        extendBody: true,
         backgroundColor: Colors.transparent,
-        body: screens[selectedIndex],
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: selectedIndex,
-          backgroundColor: Colors.black.withOpacity(0),
-          elevation: 0,
-          selectedItemColor: AppColors.selectedItemColor,
-          unselectedItemColor: AppColors.unselectedItemColor,
-          type: BottomNavigationBarType.fixed,
-          onTap: (index) {
-            setState(() {
-              selectedIndex = index;
-            });
-          },
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: "Home",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.newspaper),
-              label: "News",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.bar_chart),
-              label: "Stocks",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.pie_chart),
-              label: "Portfolio",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: "Profile",
-            ),
-          ],
+          body: Stack(
+            children: [
+              screens[selectedIndex],
+
+              Positioned(
+                left: 20,
+                right: 20,
+                bottom: 20,
+                child: Container(
+                  height: 80,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(40),
+                    gradient: const LinearGradient(
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
+                      colors: [
+                        AppColors.backgroundColor,
+                        AppColors.backgroundLightingColor,
+                      ],
+                    ),
+                    border: Border.all(
+                      color: Colors.white24,
+                      width: 1,
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.35),
+                        blurRadius: 20,
+                        offset: const Offset(0, 10),
+                      ),
+                    ],
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      _buildNavItem(Icons.home_rounded, 0),
+                      _buildNavItem(Icons.newspaper_rounded, 1),
+                      _buildNavItem(Icons.bar_chart_rounded, 2),
+                      _buildNavItem(Icons.pie_chart_rounded, 3),
+                      _buildNavItem(Icons.person_rounded, 4),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+      ),
+    );
+  }
+
+  Widget _buildNavItem(IconData icon, int index) {
+    final bool isSelected = selectedIndex == index;
+
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          selectedIndex = index;
+        });
+      },
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 200),
+        padding: const EdgeInsets.all(8),
+        child: Icon(
+          icon,
+          size: 34,
+          color: isSelected
+              ? Colors.white
+              : Colors.white.withOpacity(0.7),
         ),
       ),
     );
