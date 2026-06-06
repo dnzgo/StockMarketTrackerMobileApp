41f75473469e0086961eff78c412f65efb34bf3c
dnzgo <dnzgzc.98@gmail.com>
2026-06-06 18:32:01 +0200

    small stock chip for news detail
    news detail screen created,
    for affected stocks a small chip created

---
+++ lib/widgets/affected_stock_chip.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import '../utils/app_theme.dart';
+
+class AffectedStockChip extends StatelessWidget{
+  final String symbol;
+  final double changePercentage;
+  final bool isPositive;
+  final VoidCallback onTap;
+
+  const AffectedStockChip({
+    super.key,
+    required this.symbol,
+    required this.changePercentage,
+    required this.isPositive,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        width: 90,
+        padding: EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          color: Colors.white12,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(symbol),
+            Text(
+              "${changePercentage.toStringAsFixed(2)}%",
+              style: TextStyle(
+                color: isPositive
+                    ? AppColors.increasedValueColor
+                    : AppColors.decreasedValueColor,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+}
